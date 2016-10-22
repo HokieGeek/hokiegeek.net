ce05f50a6d3e406de9200cb748b533fd6fd2f3e6
HokieGeek <andres.f.perez@gmail.com>
2016-10-21 22:09:11 -0400

    Smarter post-install script

--- .bower-postinstall.sh
+++ .bower-postinstall.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 for d in hg-*; do
-    pushd $d >/dev/null
-    npm install && [ -f typings.json ] && npm run typings install
-    tsc
+    pushd $d
+    [ -f package.json ] && {
+        npm install
+        [ -f typings.json ] && npm run typings install
+    }
+    [ `find . -name "*.ts" -print | wc -l` -gt 0 ] && tsc
     popd
 done
