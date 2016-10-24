8f26fdeba9022fc66743e4e477f336b06fa3c172
HokieGeek <andres.f.perez@gmail.com>
2016-10-24 16:57:27 -0400

    Added the tea component correctly

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,18 +5,14 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent }   from './app.component';
 import { HomeComponent }  from './home.component';
 
-import { HgTeaModule }    from 'hg-tea/app/app.modules';
-// import { HgTeaModule }    from 'hg-tea';
-// import { HgHouseModule }  from './hg-house/app/app.module';
-
+import { HgTeaModule }    from 'hg-tea/app/app.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HgTeaModule,
-    // HgHouseModule,
     RouterModule.forRoot([
-        { path: '', component: HomeComponent }
+        { path: '', component: HomeComponent },
     ])
   ],
   declarations: [
